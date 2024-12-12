c604aeb543bf6b9afda7373549fb30d7638c60b3
mfarhanr <muhammadfarhanr09@gmail.com>
2024-12-12 11:54:40 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,14 +1,7 @@
 import java.util.Scanner;
 
 public class Nomor1 {
-
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-       System.out.print("Masukan nilai N : ");
-        fizzBuzz(N);
-     static void fizzBuzz(int n) {
+    static void fizzBuzz(int n) {
         for (int i = 0; i <= n; i++) {
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.print("FizzBuzz ");
@@ -19,11 +12,20 @@ public class Nomor1 {
             } else {
                 System.out.print(i + " ");
             }
-        }
+        }    
     }
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        /* Pekerjaan anda mulai dari sini */
+
+        System.out.println("Masukan nilai N : ");
+        fizzBuzz(N);
+        
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
-    }
     
+    }
 }
